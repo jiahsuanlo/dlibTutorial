82b767dc9b3c8a18cc3a1ee076715c65d27441aa
jlo <j.lo@hemexhealth.com>
2019-09-08 20:52:57 -0700

    add trainer settings, training, and save model buttons

--- DigitRecognition/digitrecognitionapp.cpp
+++ DigitRecognition/digitrecognitionapp.cpp
@@ -3,7 +3,8 @@
 
 DigitRecognitionApp::DigitRecognitionApp(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::DigitRecognitionApp)
+    ui(new Ui::DigitRecognitionApp),
+    digitpred_(dp::DigitPredictorParams())
 {
     ui->setupUi(this);
 }
@@ -15,7 +16,97 @@ DigitRecognitionApp::~DigitRecognitionApp()
 
 void DigitRecognitionApp::on_pushButtonStart_clicked()
 {
-    dp::DigitPredictor digitpred;
+    digitpred_.Train();
 
-    digitpred.Test("./data");
+    ui->pushButtonSaveModel->setEnabled(true);
+}
+
+void DigitRecognitionApp::on_pushButtonSelectDirectory_clicked()
+{
+    QString in_dir= QFileDialog::getExistingDirectory(this, tr("Select Mnist Data Folder"));
+
+    // load data now
+    digitpred_.LoadData(in_dir);
+
+    // show on line edit
+    ui->lineEditDataDirectory->setText(in_dir);
+
+    // enable gui
+    ui->groupBoxTrainer->setEnabled(true);
+    ui->pushButtonStart->setEnabled(true);    
+}
+
+void DigitRecognitionApp::on_lineEditLearnRate_textChanged(const QString &arg1)
+{
+    dp::DigitPredictorParams &params= digitpred_.Params();
+    bool ok;
+    double newlr= arg1.toDouble(&ok);
+    if (ok)
+    {
+        params.learning_rate= newlr;
+        digitpred_.SetTrainerSettings();
+    }
+}
+
+void DigitRecognitionApp::on_lineEditMiniLearnRate_textChanged(const QString &arg1)
+{
+    dp::DigitPredictorParams &params= digitpred_.Params();
+    bool ok;
+    double newmlr= arg1.toDouble(&ok);
+    if (ok)
+    {
+        params.mini_learning_rate= newmlr;
+        digitpred_.SetTrainerSettings();
+    }
+}
+
+void DigitRecognitionApp::on_lineEditMiniBatchSize_textChanged(const QString &arg1)
+{
+    dp::DigitPredictorParams &params= digitpred_.Params();
+    bool ok;
+    unsigned long newbs= arg1.toULong(&ok);
+    if (ok)
+    {
+        params.minibatch_size= newbs;
+        digitpred_.SetTrainerSettings();
+    }
+}
+
+void DigitRecognitionApp::on_lineEditMnistSync_textChanged(const QString &arg1)
+{
+    dp::DigitPredictorParams &params= digitpred_.Params();
+    params.sync_filename= arg1.toStdString();
+    digitpred_.SetTrainerSettings();
+}
+
+
+
+void DigitRecognitionApp::on_checkBoxVerbose_toggled(bool checked)
+{
+    dp::DigitPredictorParams &params= digitpred_.Params();
+    params.trainer_verbose= checked;
+    digitpred_.SetTrainerSettings();
+}
+
+void DigitRecognitionApp::on_lineEditSyncSec_textChanged(const QString &arg1)
+{
+    dp::DigitPredictorParams &params= digitpred_.Params();
+    bool ok;
+    int newsec= arg1.toInt(&ok);
+    if (ok)
+    {
+        params.sync_sec= newsec;
+        digitpred_.SetTrainerSettings();
+    }
+}
+
+void DigitRecognitionApp::on_pushButtonSaveModel_clicked()
+{
+    QString filename= QFileDialog::getSaveFileName(this,
+                                 tr("Save Model File"),
+                                 "",
+                                 tr("Data file (*.dat)"));
+
+    digitpred_.SaveModel(filename);
+
 }
--- DigitRecognition/digitrecognitionapp.h
+++ DigitRecognition/digitrecognitionapp.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include "digitpredictor.h"
+#include <QFileDialog>
 
 namespace Ui {
 class DigitRecognitionApp;
@@ -19,8 +20,27 @@ public:
 private slots:
     void on_pushButtonStart_clicked();
 
+    void on_pushButtonSelectDirectory_clicked();
+
+    void on_lineEditLearnRate_textChanged(const QString &arg1);
+
+    void on_lineEditMiniLearnRate_textChanged(const QString &arg1);
+
+    void on_lineEditMiniBatchSize_textChanged(const QString &arg1);
+
+    void on_lineEditMnistSync_textChanged(const QString &arg1);
+
+    void on_checkBoxVerbose_toggled(bool checked);
+
+    void on_lineEditSyncSec_textChanged(const QString &arg1);
+
+    void on_pushButtonSaveModel_clicked();
+
 private:
     Ui::DigitRecognitionApp *ui;
+
+    // ==== digit recognition ====
+    dp::DigitPredictor digitpred_;
 };
 
 #endif // DIGITRECOGNITIONAPP_H
